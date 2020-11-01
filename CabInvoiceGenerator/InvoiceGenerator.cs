eff752b9346c0b224a648498fac2c58250d4cf7a
Swapnil <schakravarti930@gmail.com>
2020-11-01 15:52:42 +0530

    [Swapnil] Add : Calculate Fare for a ride

---
+++ CabInvoiceGenerator/InvoiceGenerator.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace CabInvoiceGenerator
+{
+    public class InvoiceGenerator
+    {
+        public double CalculateFare(double distance,int time)
+        {
+            int costPerKilometer = 10;
+            int costPerMinute = 1;
+            int minimumFare = 5;
+            double totalFare = distance * costPerKilometer + time * costPerMinute;
+            if (totalFare < minimumFare)
+                return minimumFare;
+            return totalFare;
+        }
+    }
+}
---
+++ CabInvoiceGenerator/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CabInvoiceGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Cab Invoice Generator");
+            InvoiceGenerator invoice = new InvoiceGenerator();
+            Console.WriteLine(invoice.CalculateFare(2.0,5));
+        }
+    }
+}
