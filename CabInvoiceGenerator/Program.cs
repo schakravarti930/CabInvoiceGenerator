49a6f1675e0381f755083b3ba02b797d921bd2d3
schakravarti <schakravarti930@gmail.com>
2020-11-03 11:16:29 +0530

    [Swapnil] Add : Premium Invoice Service

--- CabInvoiceGenerator/Program.cs
+++ CabInvoiceGenerator/Program.cs
@@ -6,9 +6,10 @@ namespace CabInvoiceGenerator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Cab Invoice Generator");
-            InvoiceGenerator invoice = new InvoiceGenerator();
-            Console.WriteLine(invoice.CalculateFare(2.0,5));
+           Console.WriteLine("Welcome to Cab Invoice Generator!");
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+            double fare = invoiceGenerator.CalculateFare(2.0, 5);
+            Console.WriteLine($"Fare : {fare}");
         }
     }
 }
---
+++ CabInvoiceGenerator/RideType.cs
@@ -0,0 +1,4 @@
+namespace CabInvoiceGenerator
+{
+    public enum RideType { NORMAL, PREMIUM }
+}
